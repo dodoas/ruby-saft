d52edbb1c2a7a67cdabb833daf86db24e0f2498f
Simon <simon.toivo@telhaug.no>
2022-12-07 08:36:19 +0100

    Allow users to eager_load_all
    I want to allow eager loading since it allows the app to use process to load
    files one and then copy the memory with COW so it uses less memory. The html
    view requires tubby which is an optional dependency so I don't want to load it
    unless you specifically require it.

--- lib/saft.rb
+++ lib/saft.rb
@@ -7,8 +7,10 @@ loader = Zeitwerk::Loader.for_gem
 loader.inflector.inflect("saft" => "SAFT", "html" => "HTML")
 loader.setup
 
+loader.do_not_eager_load("#{__dir__}/saft/v2/html.rb")
+
 module SAFT
-  def self.nokogiri_save_setting= value
+  def self.nokogiri_save_setting=(value)
     @nokogiri_save_setting = value
   end
 
